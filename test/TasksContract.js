c6c84ea056df6f3f1aac0f4723aac3e086492f76
juliswer <julianswer36@gmail.com>
2022-05-03 11:48:11 -0300

    testing

--- test/TasksContract.js
+++ test/TasksContract.js
@@ -1,18 +1,24 @@
-const TasksContract = artifacts.require("TasksContract")
+const TasksContract = artifacts.require("TasksContract");
 
 contract("TasksContract", () => {
-    before(async () => {
-        this.tasksContract = await TasksContract.deployed()
-    })
+  before(async () => {
+    this.tasksContract = await TasksContract.deployed();
+  });
 
-    it("migrate deployed succesfully", async () => {
-        const address = this.tasksContract.address;
+  it("migrate deployed succesfully", async () => {
+    const address = this.tasksContract.address;
 
-        assert.notEqual(address, null);
-        assert.notEqual(address, undefined);
-        assert.notEqual(address, 0x0);
-        assert.notEqual(address, "");
+    assert.notEqual(address, null);
+    assert.notEqual(address, undefined);
+    assert.notEqual(address, 0x0);
+    assert.notEqual(address, "");
+  });
 
-    })
+  it("Get tasks List", async () => {
+    const taskCounter = await this.tasksContract.taskCounter();
+    const task = await this.tasksContract.tasks(taskCounter - 1);
 
-})
+    assert.equal(task.id.toNumber(), taskCounter - 1);
+
+  });
+});
