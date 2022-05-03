03330c30fcd1705e9401fccc6ed52b41b30645f1
juliswer <julianswer36@gmail.com>
2022-05-03 11:42:00 -0300

    testing

---
+++ test/TasksContract.js
@@ -0,0 +1,18 @@
+const TasksContract = artifacts.require("TasksContract")
+
+contract("TasksContract", () => {
+    before(async () => {
+        this.tasksContract = await TasksContract.deployed()
+    })
+
+    it("migrate deployed succesfully", async () => {
+        const address = this.tasksContract.address;
+
+        assert.notEqual(address, null);
+        assert.notEqual(address, undefined);
+        assert.notEqual(address, 0x0);
+        assert.notEqual(address, "");
+
+    })
+
+})
