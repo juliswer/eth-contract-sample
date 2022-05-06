57a65973bcf548cb4982af5a2b6d6234cb1a79b3
juliswer <julianswer36@gmail.com>
2022-05-06 16:11:16 -0300

    metamask and load contracts

--- client/app.js
+++ client/app.js
@@ -1,17 +1,24 @@
 App = {
   init: () => {
     App.loadEthereum();
+    App.loadContracts()
   },
 
   loadEthereum: async () => {
     if (window.ethereum) {
       App.web3Provider = window.ethereum;
-      await window.ethereum.request({ method: "eth_requestAccounts" });
+      await ethereum.request({ method: "eth_requestAccounts" });
     } else if (window.web3) {
       web3 = new Web3(window.web3.currentProvider);
     } else {
       console.log("No web3? You should consider trying MetaMask!");
     }
+  },
+
+  loadContracts: async () => {
+    const res = await fetch("TasksContract.json");
+    const tasksContractJSON = await res.json();
+    console.log(tasksContractJSON);
   },
 };
 
