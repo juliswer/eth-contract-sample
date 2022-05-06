dfe2b19579a28d79771289bef38bda438de38f04
juliswer <julianswer36@gmail.com>
2022-05-06 16:00:46 -0300

    metamask

--- client/app.js
+++ client/app.js
@@ -1,15 +1,16 @@
 App = {
-  web3Provider: "",
-
   init: () => {
-    console.log("Loaded");
     App.loadEthereum();
   },
 
   loadEthereum: async () => {
-    if(window.ethereum) {
-        App.web3Provider = window.ethereum;
-        await window.ethereum.request({method: 'eth_requestAccounts'});
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+    } else if (window.web3) {
+      web3 = new Web3(window.web3.currentProvider);
+    } else {
+      console.log("No web3? You should consider trying MetaMask!");
     }
   },
 };
